1c31ec16c6b9fdc3b7a83e093301a217836f4e3b
Zahid-101 <zahidwork908@gmail.com>
2025-02-01 20:36:08 +0530

    Added some adjustments for a registration form for members made a new page to select between resgistration or logging in Edited the program.cs file to switch into the new page

---
+++ Library Management System/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // Hide current form
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // Hide current form
+            Register registerForm = new Register();
+            registerForm.Show();
+        }
+    }
+}

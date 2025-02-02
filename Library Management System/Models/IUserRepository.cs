215f7c4238b8c910f702617919e1071fe72f2699
Zahid-101 <zahidwork908@gmail.com>
2025-02-02 08:59:32 +0530

    Made the interface that supports the updating of and adding of members , managing of fines for overdue books

---
+++ Library Management System/Models/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management_System.Models
+{
+    public interface IUserRepository
+    {
+        Task<Member> CreateMember(Member member);
+        Task<Member> UpdateMember(Member member);
+        Task<Librarian> CreateLibrarian(Librarian librarian);
+        Task<User> GetUserById(int userId);
+        Task<Member> GetMemberById(int memberId);
+        Task UpdateUser(User user);
+        Task<List<Member>> GetMembersWithOutstandingFines();
+    }
+}
